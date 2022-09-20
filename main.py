da385a418f154710818423c93ffeb4deb7d0ed73
ricepotato <sukjun40@naver.com>
2022-09-21 02:00:05 +0900

    main entry

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+import dotenv
+from pay.credit_card import CreditCard
 from pay.order import LineItem, Order
 from pay.payment import pay_order
+from pay.processor import PaymentProcessor
+
+dotenv.load_dotenv()
+
+API_KEY = os.getenv("API_KEY") or ""
 
 
 def main() -> None:
@@ -7,10 +15,12 @@ def main() -> None:
     month = int(input("Please enter the card expiry month: "))
     year = int(input("Please enter the card expiry year: "))
 
+    credit_card = CreditCard(card, month, year)
+
     order = Order()
     order.line_items.append(LineItem(name="shoes", price=100_00, quantity=2))
     order.line_items.append(LineItem(name="Gat", price=50_00))
-    pay_order(order)
+    pay_order(order, credit_card, PaymentProcessor(API_KEY))
 
 
 if __name__ == "__main__":
